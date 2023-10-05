0542f00d48f29b060ba446fd0909a9a8f550fca9
tylersmith <tyjsmith1@gmail.com>
2023-10-05 08:13:32 -0500

    completed code in index.js for objects lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+const employee = {
+    name: "Tyler Smith",
+    streetAddress: "123 Fee Lane",
+};
+
+function updateEmployeeWithKeyAndValue() {
+    const newEmployee = {
+        ...employee,
+        name: "Sam",
+        streetAddress: "11 Broadway",
+    };
+    return newEmployee;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue() {
+    employee.name = "Sam";
+    employee.streetAddress = "12 Broadway";
+    return employee;
+};
+
+function deleteFromEmployeeByKey() {
+    const newEmployeeTwo = {
+        ...employee,
+
+    };
+    delete newEmployeeTwo.name;
+    return newEmployeeTwo;
+};
+
+function destructivelyDeleteFromEmployeeByKey() {
+    delete employee.name;
+    return employee;
+}
